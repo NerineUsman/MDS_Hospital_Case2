7992ecc15d8ad9dc63a60ef8f88a777e23361189
AlvaroGI <alvarogomezinesta@gmail.com>
2019-04-27 22:51:30 +0200

    More detailed preprocessing: number of features reduced from 136 to 36

--- aux_preprocessing.py
+++ aux_preprocessing.py
@@ -93,7 +93,9 @@ def get_feature_value(var,feat):
     casted into real numbers.
        ---Inputs---
        var: variable of type pydicom.dataset.FileDataset (slice)
-       feat: feature whose value we want to retrieve'''
+       feat: feature whose value we want to retrieve
+       ---Outputs---
+       Returns (multi- or uni-dimensional) feature as a list.'''
     val = var.data_element(feat).value
 
     # Numerical features:
@@ -108,15 +110,175 @@ def get_feature_value(var,feat):
         if units=='D':
             return [float(val[0:3])/365]
     if var.data_element(feat).VR=='DA':
-        return [val[0:4], val[4:6], val[6:8]]
+        if feat=='DateOfLastCalibration':
+            calibdate = int(val[4:6])*30+int(val[6:8])
+            acval = var.data_element('AcquisitionDate').value
+            actime = int(acval[4:6])*30+int(acval[6:8])
+            return [actime-calibdate]
+        else:
+            return [int(val[4:6])*30+int(val[6:8])]
     if var.data_element(feat).VR=='DS':
         if isinstance(val, pydicom.multival.MultiValue): # list of ints
-            return [int(val_) for val_ in val]
+            val = [int(val_) for val_ in val]
         else: # single int
-            return [int(val), 1] # The DS elements are floats expressed as a
+            val = [int(val), 1] # The DS elements are floats expressed as a
                                  # ratio of two ints. If the number is a single
                                  # int, it corresponds to that number divided
                                  # by 1.
+        if feat in ['ContrastBolusTotalDose','ContrastBolusVolume','ContrastFlowDuration','ContrastFlowRate']:
+            if val in [[125, 1],[30, 14],[3, 2]]:
+                return [0]
+            if val in [[100, 1],[25, 1],[3, 1]]:
+                return [1]
+            else:
+                print('ERROR: '+feat+' has unrecognized value.')
+        elif feat in ['KVP','WindowCenter','WindowWidth']:
+            if val in [[100, 1],[60, -500],[375, 1500]]:
+                return [0]
+            if val in [[120, 1],[50, -500],[350, 1500]]:
+                return [1]
+            else:
+                print('ERROR: '+feat+' has unrecognized value:')
+        elif feat in ['ReconstructionDiameter', 'SliceLocation', 'TableHeight']:
+            return [val[0]]
+        else:
+            return val
+    if var.data_element(feat).VR=='DT':
+        return [val[0:4], val[4:6], val[6:8], val[8:10], val[10:12], val[12:14],
+                val[15:21]]
+    if var.data_element(feat).VR=='FD':
+        if feat=='DataCollectionCenterPatient':
+            vall = [float(val_) for val_ in val]
+            return vall[1:]
+        if isinstance(val, list): # list of floats
+            return [float(val_) for val_ in val]
+        else: # single float
+            return [float(val)]
+    if var.data_element(feat).VR=='FL':
+        if feat=='CalciumScoringMassFactorDevice':
+            if var.data_element(feat).value==[0.6430000066757202, 0.6710000038146973, 0.6980000138282776]:
+                return [0]
+            if var.data_element(feat).value==[0.7429999709129333, 0.7789999842643738, 0.8119999766349792]:
+                return [1]
+            else:
+                print('ERROR: CalciumScoringMassFactorDevice has unrecognized value.')
+        if isinstance(val, list): # list of floats
+            return [float(val_) for val_ in val]
+        else: # single float
+            return [float(val)]
+    if var.data_element(feat).VR=='IS':
+        if isinstance(val, list): # list of ints
+            return [int(val_) for val_ in val]
+        else: # single int
+            return [int(val)]
+    if var.data_element(feat).VR=='TM':
+        if feat=='ContrastBolusStartTime':
+            starttime = float(val[4:6])+float(val[2:4])*60+float(val[0:2])*3600
+            stopval = var.data_element('ContrastBolusStopTime').value
+            stoptime = float(stopval[4:6])+float(stopval[2:4])*60+float(stopval[0:2])*3600
+            if int(stoptime-starttime)==74:
+                return [0]
+            elif int(stoptime-starttime)==26:
+                return [1]
+        elif feat=='ContrastBolusStopTime':
+            print('ContrastBolusStopTime should be ignored (not implemented).')
+        elif feat=='TimeOfLastCalibration':
+            starttime = float(val[2:4])/60+float(val[0:2])
+            stopval = var.data_element('AcquisitionTime').value
+            stoptime = float(stopval[2:4])/60+float(stopval[0:2])
+            return [stoptime-starttime]
+        elif feat=='ContentTime':
+            starttime = float(val[4:6])+float(val[2:4])*60
+            stopval = var.data_element('AcquisitionTime').value
+            stoptime = float(stopval[4:6])+float(stopval[2:4])*60
+            return [starttime-stoptime]
+        else:
+            return [float(val[0:2])+float(val[2:4])/60]
+    if var.data_element(feat).VR=='US':
+        if isinstance(val, list): # list of ints
+            return [int(val_) for val_ in val]
+        else: # single int
+            return [int(val)]
+
+    # String features:
+    if (var.data_element(feat).VR=='CS') or (var.data_element(feat).VR=='LO') or (var.data_element(feat).VR=='SH'):
+        if feat=='ConvolutionKernel':
+            if var.data_element(feat).value[0]=='I40f':
+                return [0]
+            if var.data_element(feat).value[0]=='I26f':
+                return [1]
+            else:
+                print('ERROR: ConvolutionKernel has unrecognized value.')
+        if feat=='PatientSex':
+            if var.data_element(feat).value=='F':
+                return [0]
+            if var.data_element(feat).value=='M':
+                return [1]
+            else:
+                print('ERROR: PatientSex has unrecognized value.')
+        else:
+            print('String feature ignored.')
+
+    # Sequence features:
+    if var.data_element(feat).VR=='SQ':
+        seq = var.data_element(feat)[0]
+
+        if (feat=='ProcedureCodeSequence') or (feat=='RequestedProcedureCodeSequence'):
+            if seq.data_element('CodeValue').value=='C5-05':
+                return [0]
+            if seq.data_element('CodeValue').value=='C5-01':
+                return [1]
+            else:
+                print('ERROR: CodeValue subfeature has unrecognized value.')
+        else:
+            print('Sequence feature ignored.')
+
+    # Special features:
+    if var.data_element(feat).VR=='OW':
+        print('Not implemented')
+
+    if var.data_element(feat).VR=='UI':
+        print('UID feature ignored.')
+
+def get_feature_value_numerSection(var,feat):
+    '''Gets the value of a feature of a particular slice. All the values are
+    casted into real numbers. THIS IS A TEST FUNCTION THAT IS ONLY USED IN
+    THE preprocessing_CT.ipynb FILE. DO NOT USE.
+       ---Inputs---
+       var: variable of type pydicom.dataset.FileDataset (slice)
+       feat: feature whose value we want to retrieve
+       ---Outputs---
+       Returns (multi- or uni-dimensional) feature as a list.'''
+    val = var.data_element(feat).value
+
+    # Numerical features:
+    if var.data_element(feat).VR=='AS':
+        units = val[-1]
+        if units=='Y':
+            return [float(val[0:3])]
+        if units=='M':
+            return [float(val[0:3])/12]
+        if units=='W':
+            return [float(val[0:3])*7/365]
+        if units=='D':
+            return [float(val[0:3])/365]
+    if var.data_element(feat).VR=='DA':
+        if feat=='DateOfLastCalibration':
+            calibdate = int(val[4:6])*30+int(val[6:8])
+            acval = var.data_element('AcquisitionDate').value
+            actime = int(acval[4:6])*30+int(acval[6:8])
+            return [actime-calibdate]
+        else:
+            return [int(val[4:6])*30+int(val[6:8])]
+    if var.data_element(feat).VR=='DS':
+        if isinstance(val, pydicom.multival.MultiValue): # list of ints
+            val = [int(val_) for val_ in val]
+        else: # single int
+            val = [int(val), 1] # The DS elements are floats expressed as a
+                                 # ratio of two ints. If the number is a single
+                                 # int, it corresponds to that number divided
+                                 # by 1.
+        return val
     if var.data_element(feat).VR=='DT':
         return [val[0:4], val[4:6], val[6:8], val[8:10], val[10:12], val[12:14],
                 val[15:21]]
@@ -136,7 +298,7 @@ def get_feature_value(var,feat):
         else: # single int
             return [int(val)]
     if var.data_element(feat).VR=='TM':
-        return [val[0:2], val[2:4], val[4:6], val[5:11]]
+        return [float(val[0:2])+float(val[2:4])/60]
     if var.data_element(feat).VR=='US':
         if isinstance(val, list): # list of ints
             return [int(val_) for val_ in val]
